8e8e654b4b38ae53d1553827ac62e7f8fddb28f6
Cabbagelol <nickmiku@foxmail.com>
2024-10-14 03:13:03 +0800

    v0.1.2+6

--- src/components/header.vue
+++ src/components/header.vue
@@ -2,14 +2,14 @@
   <v-app-bar elevation="0" floating absolute>
     <v-container max-width="1200">
       <v-row align="center">
-        <v-btn icon>
+        <v-btn stacked>
           <v-img src="/header_logo.webp" alt="header" width="15px" height="25px"/>
         </v-btn>
         <v-list class="d-block ml-lg-5 ml-sm-0" tile>
           <v-btn variant="text" to="/">
             首页
           </v-btn>
-          <v-btn variant="text" to="/all-calc" class="ml-1 mr-1">
+          <v-btn variant="text" to="/all-calc" class="ml-lg-1 mr-lg-1">
             算法集
           </v-btn>
           <v-btn variant="text" to="/about">
@@ -17,9 +17,7 @@
           </v-btn>
         </v-list>
         <v-spacer></v-spacer>
-        <v-btn variant="tonal" href="https://github.com/cabbagelol/remnant-2-dlc-origin-bow" target="_blank">
-          <span>github</span>
-          <v-icon class="ml-2">mdi-open-in-new</v-icon>
+        <v-btn icon="mdi-github" class="mr-1" density="comfortable" variant="tonal" href="https://github.com/cabbagelol/remnant-2-dlc-origin-bow" target="_blank">
         </v-btn>
       </v-row>
     </v-container>
